290edd66bc443fec0b8c376bdbd6c5feaf8edc16
Nikitashev1327 <34217490+Nikitashev1327@users.noreply.github.com>
2021-03-28 12:29:43 +0300

    Create Dot.java

---
+++ Lab2/Dot.java
@@ -0,0 +1,20 @@
+public class Dot {
+    private double x;
+    private double y;
+    public Dot (double a_x, double a_y){
+        x = a_x;
+        y = a_y;
+    }
+    public double getX(){
+        return x;
+    }
+    public double getY(){
+        return y;
+    }
+    public void setX(double a_x){
+        x = a_x;
+    }
+    public void setY(double a_y){
+        y = a_y;
+    }
+}
